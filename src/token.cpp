5e83f408143482037c1dd998a6654a0440163831
Lee Thomason <leethomason@gmail.com>
2024-10-02 20:38:38 -0700

    tricky

--- src/token.cpp
+++ src/token.cpp
@@ -2,12 +2,36 @@
 
 #include "token.h"
 
-Token Tokenizer::get_next_token() {
-    while (_current) {
-        if (std::isspace(_current)) {
-            skip_whitespace();
-            continue;
+size_t Tokenizer::readNumber(size_t start)
+{
+    if (isNumberStart(_input[start])) {
+        start++;
+        while(start < _input.size() && isNumber(_input[start])) {
+			start++;
+		}
+    }
+    return start;
+}
+
+Token Tokenizer::getNext(uint32_t classFlags)
+{
+    skipWhitespace();
+    if (_pos == _input.size()) {
+		return Token(TokenType::eof);
+	}
+
+    if (classFlags & cNumber) {
+        size_t end = readNumber(_pos);
+        if (end != _pos) {
+            std::string number = _input.substr(_pos, end - _pos);
+            _pos = end;
+            return Token(TokenType::number, number);
         }
+    }
+
+
+    while (_pos < _input.size()) {
+
 
         if (std::isdigit(_current)) {
             std::string number;
--- src/token.h
+++ src/token.h
@@ -2,7 +2,6 @@
 
 #include <string>
 
-// Token types
 enum class TokenType {
     eof,        // End of file/input
     number,     // Number
@@ -28,22 +27,45 @@ struct Token {
 // Tokenizer to break input into tokens
 class Tokenizer {
 public:
-    Tokenizer(std::string input) : _input(input), _pos(0), _current(input[0]) {}
-    Token get_next_token();
+    enum {
+        cNumber = 0x01,
+        cIdent = 0x02,
+        cOperator = 0x04,
+    };
+
+    Tokenizer(const std::string& input) : _input(input) {}
+    Token getNext(uint32_t classFlags);
 
 private:
-    std::string _input;
+    const std::string& _input;
     size_t _pos = 0;
-    char _current = 0;
 
     void advance() {
         _pos++;
-        _current = (_pos < _input.length()) ? _input[_pos] : 0;
     }
 
-    void skip_whitespace() {
-        while (std::isspace(_current)) {
+    void skipWhitespace() {
+        while (std::isspace(_pos)) {
             advance();
         }
     }
+
+    static bool isDigit(char c) {
+		return c >= '0' && c <= '9';
+	}
+    static bool isNumber(char c) {
+        return isDigit(c) || c == '.';
+	}
+    static bool isNumberStart(char c) {
+        return isNumber(c) || c == '-' || c == '+';
+    }
+    static bool isIdent(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_' || (c >= '0' && c <= '9');
+    }
+    static bool isIdentStart(char c) {
+		return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+	}
+
+    size_t readIdentifier(size_t start);
+    size_t readNumber(size_t start);
 };
